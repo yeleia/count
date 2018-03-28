813b153daf03d4536d3e82b0dd5454a7879c43b4
yeleia <2406865312@qq.com>
2018-03-28 16:50:12 +0800

    搭建环境

--- src/main/java/cn/sicau/count/controller/Test.java
+++ src/main/java/cn/sicau/count/controller/Test.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class Test {
-    @RequestMapping("/test")
+   /* @RequestMapping("/test")
     @ResponseBody
     public String test(){
         return "dfdgj";
-    }
+    }*/
 }
