49233946aeba5e09d7d1461dc7638c5b5b569d10
yeleia <2406865312@qq.com>
2018-03-28 16:57:41 +0800

    搭建环境

---
+++ src/main/java/cn/sicau/count/controller/MainController.java
@@ -0,0 +1,11 @@
+package cn.sicau.count.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author yelei
+ * @date 18-3-28
+ */
+@Controller
+public class MainController {
+}
